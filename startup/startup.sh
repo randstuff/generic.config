7975c26ce1bcf39e8f1f6ebf2445d26856f4803e
randstuff <n@no.no>
2019-05-31 01:26:32 +0200

    .

---
+++ startup/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh 
+
+tint2 & 
+feh --bg-tile $HOME/background/80961.jpg &  
+conky -b -dq -c $HOME/_config/conky.fnix/box.left.01 &
+conky -b -dq -c $HOME/_config/conky.fnix/box.left.02 &
+conky -b -dq -c $HOME/_config/conky.fnix/box.right.01 &
+
+
