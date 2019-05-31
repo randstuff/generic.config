7f1c67689cd08f94cef4df0703ab33975c02fa84
randstuff <n@no.no>
2019-05-31 02:52:30 +0200

    .

--- startup/startup.sh
+++ startup/startup.sh
@@ -4,6 +4,9 @@ tint2 &
 feh --bg-tile $HOME/background/80961.jpg &  
 conky -b -dq -c $HOME/_config/conky.fnix/box.left.01 &
 conky -b -dq -c $HOME/_config/conky.fnix/box.left.02 &
+conky -b -dq -c $HOME/_config/conky.fnix/box.left.03 &
+conky -b -dq -c $HOME/_config/conky.fnix/box.left.04 &
+conky -b -dq -c $HOME/_config/conky.fnix/box.left.05 &
 conky -b -dq -c $HOME/_config/conky.fnix/box.right.01 &
 
 
